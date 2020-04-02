e168226c5f39f29a2a62efa21abdfa45530b86e0
Mingxuan Fan <mfan@ucsd.edu>
2020-04-01 20:53:31 -0700

    Update headers file

---
+++ constants.h
@@ -0,0 +1,19 @@
+#ifndef LOCALHOST
+#define LOCALHOST "127.0.0.1"
+#endif
+
+#ifndef DEFAULT_PORT
+#define DEFAULT_PORT 8888
+#endif
+
+#ifndef DEFAULT_MAX_CLIENT
+#define DEFAULT_MAX_CLIENT 20
+#endif
+
+#ifndef PORT_EXP_LIMIT
+#define PORT_EXP_LIMIT 16
+#endif
+
+#ifndef BUF_SIZE
+#define BUF_SIZE 4096
+#endif
--- server.h
+++ server.h
@@ -8,22 +8,6 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#ifndef LOCALHOST
-#define LOCALHOST "127.0.0.1"
-#endif
-
-#ifndef DEFAULT_PORT
-#define DEFAULT_PORT 8888
-#endif
-
-#ifndef DEFAULT_MAX_CLIENT
-#define DEFAULT_MAX_CLIENT 20
-#endif
-
-#ifndef PORT_EXP_LIMIT
-#define PORT_EXP_LIMIT 16
-#endif
-
 #ifndef SERVER_F_INCLUDED
 #define SERVER_F_INCLUDED
 
